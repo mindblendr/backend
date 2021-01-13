3313a95b994035efbf3b862947ce48ede56bdd45
Aljon Cruz <mindblendr@gmail.com>
2021-01-13 20:48:27 +0800

    Changes: 12

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,11 +1,161 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
-
+class MY_Controller extends CI_Controller
+{
+    public $_data = NULL;
     public function __construct()
     {
         parent::__construct();
+		$this->_prep_data();
+
+		$exploded_uri = explode('/', substr($_SERVER['REQUEST_URI'], 1));
+		if (count($exploded_uri) > 0) {
+		
+			if ($this->input->post('session_id')) {
+				$exploded_session_id = explode('_', $this->input->post('session_id'));
+				if (count($exploded_session_id) === 2) {
+					switch($this->config->item('user_table')[$exploded_session_id[1]]) {
+						case 'admin':
+							$this->session_user = $this->admin
+															->select('admin.*')
+															->where([
+																'admin.session_id' => $this->input->post('session_id'),
+																'admin.status' => 2
+															])->get();
+							if (is_logged()) $this->session_user->type = 1;
+							break;
+						case 'agent':
+							$this->session_user = $this->agent
+															->select('agent.*')
+															->where([
+																'agent.session_id' => $this->input->post('session_id'),
+																'agent.status' => 2
+															])->get();
+							if (is_logged()) $this->session_user->type = 2;
+							break;
+					}
+				}
+			}
+
+			switch($exploded_uri[0]) {
+				case 'admin':
+					if (is_logged()) {
+						if ($this->session_user->type != 1) {
+							$this->json->_exit('Unauthorized!', 401);
+						}
+					} else {
+						$this->json->_exit('Unauthorized!', 401);
+					}
+					break;
+				case 'agent':
+					if (is_logged()) {
+						if ($this->session_user->type != 2) {
+							$this->json->_exit('Unauthorized!', 401);
+						}
+					} else {
+						$this->json->_exit('Unauthorized!', 401);
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		cors();
     }
 
+    private function _prep_data()
+    {
+        // Sanitize all POST variable
+        if ($this->input->post()) {
+            // $_POST = array_map('trim', $_POST);
+            foreach ($_POST as $key => $value) {
+                if ($_POST[$key] == null or strlen($_POST[$key]) == 0) {
+                    unset($_POST[$key]);
+                }
+            }
+        }
+
+        // Sanitize all GET variable
+        if ($this->input->get()) {
+            // $_GET = array_map('trim', $_GET);
+            foreach ($_GET as $key => $value) {
+                if ($_GET[$key] == null or strlen($_GET[$key]) == 0) {
+                    unset($_GET[$key]);
+                }
+
+            }
+		}
+		
+		if ($this->input->raw_input_stream) {
+			$raw_input = json_decode($this->security->xss_clean($this->input->raw_input_stream), TRUE, 512, JSON_OBJECT_AS_ARRAY);
+			if ($raw_input) {
+				$_POST = array_merge($_POST, $raw_input);
+			}
+		}
+    }
+
+    public function data_validation($config, $data = null, $error_convert_keys = null)
+    {
+        if (!empty($data) or !is_null($data)) {
+            $this->form_validation->set_data($data);
+        }
+
+        if (is_array($config)) {
+            foreach ($config as $key) {
+                if ($this->form_validation->run($key) == false) {
+                    $errors = $this->form_validation->error_array();
+
+                    $errors = array_values($errors);
+                    for ($i = 0; $i < count($errors); $i++) {$errors[$i] = (int) $errors[$i];}
+
+                    $this->json->_display([
+                        'context' => [
+                            'status' => array_values($errors),
+                        ],
+                        'status' => 1,
+                    ]);
+                }
+                $this->form_validation->reset_validation();
+            }
+        } else {
+            if ($this->form_validation->run($config) == false) {
+                $errors = $this->form_validation->error_array();
+
+                $errors = array_values($errors);
+                for ($i = 0; $i < count($errors); $i++) {$errors[$i] = (int) $errors[$i];}
+
+                $this->json->_display([
+                    'context' => [
+                        'status' => array_values($errors),
+                    ],
+                    'status' => 1,
+                ]);
+            }
+        }
+        $this->form_validation->reset_validation();
+    }
+
+    public function relations($query, $table, $primary_key, $foreign_key, $select = ['*'])
+    {
+        $primary_keys = [];
+        foreach ($query as $row) {
+            array_push($primary_keys, (int) $row->{$primary_key});
+        }
+
+        array_push($select, $foreign_key);
+        $this->db->select($select)
+            ->from($table)
+            ->where_in($foreign_key, $primary_keys);
+        $data = $this->db->get()->result();
+
+        foreach ($query as $row) {
+            foreach ($data as $sub) {
+                if ($row->{$primary_key} == $sub->{$foreign_key}) {
+                    $row->{$table}[] = $sub;
+                }
+            }
+        }
+    }
 }
--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -1,11 +1,322 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Model extends CI_Model {
+/**
+ * ------------------------------
+ * MY_Model - Simple overriding
+ * ------------------------------
+ * 
+ * NOTES:
+ * 
+ * Select query methods:
+ * • select()
+ * • join()
+ * • conditions()
+ * • where()
+ * • get_all()
+ * 
+ * is best executed in their sequential order as how it is written.
+ */
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+class MY_Model extends CI_Model
+{
+	public $page;
+	public $limit;
+	public $offset;
+	public $total;
+	public $no_of_pages;
+	public $soft_delete = TRUE;
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->page = $this->input->post('page') ? (int) $this->input->post('page') : 1;
+		$this->limit = $this->input->post('limit') ? (int) $this->input->post('limit') : 25;
+		$this->offset = $this->limit * ($this->page - 1);
+		$this->no_of_pages = 0;
+		$this->total = 0;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL SELECT METHOD
+	 * (same as DB->select())
+	 * -------------------------
+	 */
+	public function select($fields = '*')
+	{
+		$this->db->select($fields);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL JOIN METHOD
+	 * (same as native join)
+	 * -------------------------
+	 */
+	public function join($table, $on, $type = null)
+	{
+		if ($type) $this->db->join($table, $on, $type);
+		else $this->db->join($table, $on);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL CONDITIONS METHOD
+	 * (same as old _set_where in previous projects)
+	 * -------------------------
+	 */
+	public function conditions()
+	{
+		if ($this->input->post('date_from') && $this->input->post('date_to')) {
+			$this->db->where([
+				$this->table . '.created_at >=' => $this->input->post('date_from') . ' 00:00:00',
+				$this->table . '.created_at <=' => $this->input->post('date_to') . ' 23:59:59'
+			]);
+		}
+	
+		if ($this->input->post('column') && $this->input->post('keyword')) {
+			if (! isset($this->search_column) || in_array($this->input->post('column'), $this->search_column)) {
+				$this->db->group_start();
+				foreach (explode('|', $this->input->post('column')) as $key => $column) {
+					$this->db->or_like([$column => $this->input->post('keyword')]);
+				}
+				$this->db->group_end();
+			}
+		}
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL GROUP START
+	 * (same as native group_start)
+	 * -------------------------
+	 */
+	public function group_start()
+	{
+		$this->db->group_start();
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL GROUP START
+	 * (same as native group_start)
+	 * -------------------------
+	 */
+	public function or_group_start()
+	{
+		$this->db->or_group_start();
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL GROUP START
+	 * (same as native group_start)
+	 * -------------------------
+	 */
+	public function group_end()
+	{
+		$this->db->group_end();
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL LIKE METHOD
+	 * (same as native where)
+	 * -------------------------
+	 */
+	public function like($conditions)
+	{
+		$this->db->like($conditions);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL OR LIKE METHOD
+	 * (same as native where)
+	 * -------------------------
+	 */
+	public function or_like($conditions)
+	{
+		$this->db->or_like($conditions);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL WHERE METHOD
+	 * (same as native where)
+	 * -------------------------
+	 */
+	public function where($conditions)
+	{
+		$this->db->where($conditions);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL OR WHERE METHOD
+	 * (same as native where)
+	 * -------------------------
+	 */
+	public function or_where($conditions)
+	{
+		$this->db->or_where($conditions);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL WHERE IN METHOD
+	 * (same as native where_in)
+	 * -------------------------
+	 */
+	public function where_in($column, $values)
+	{
+		$this->db->where_in($column, $values);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * OPTIONAL GROUP BY METHOD
+	 * (same as native group_by)
+	 * -------------------------
+	 */
+	public function group_by($column)
+	{
+		$this->db->group_by($column);
+		return $this;
+	}
+
+	/**
+	 * -------------------------
+	 * SELECT ROWS - multiple
+	 * -------------------------
+	 */
+	public function get_all($paginate = TRUE, $with_trashed = FALSE, $order_by = null, $get_users = NULL)
+	{
+		$results = [];
+
+		if (! $with_trashed && $this->soft_delete) {
+			$this->db->where($this->table . '.deleted_at IS NULL');
+		}
+
+		$this->conditions();
+		if ($paginate) {
+			$tempdb = clone $this->db;
+			$this->total = $tempdb->from($this->table)->count_all_results();
+			$this->no_of_pages = (ceil($this->total / $this->limit));
+		}
+
+		$this->db->from($this->table)->limit($this->limit, $this->offset);
+		if ($order_by)
+			$this->db->order_by($order_by);
+		else if ($this->input->post('order_by_column') && $this->input->post('order_by_asc') && in_array(strtolower($this->input->post('order_by_asc')), ['asc', 'desc']))
+			$this->db->order_by($this->input->post('order_by_column') . ' ' . strtoupper($this->input->post('order_by_asc')));
+		else 
+			$this->db->order_by($this->table . '.id DESC');
+
+		$results = $this->db->get()->result();
+
+		if ($get_users && count($get_users) > 0) {
+			foreach ($results as $key => &$result) {
+				foreach ($get_users as $key => $timestamp) {
+					$timestamp_type = $timestamp . '_type';
+					$timestamp_by = $timestamp . '_by';
+					if (isset($result->$timestamp_type) && $result->$timestamp_by > 0) {
+						switch ($result->$timestamp_type) {
+							case 1:
+								$result->$timestamp = $this->select('admin.id, admin.nickname, admin.username')->admin->get($result->$timestamp_by);
+								break;
+							case 2:
+								$result->$timestamp = $this->select('agent.id, agent.nickname, agent.username')->agent->get($result->$timestamp_by);
+								break;
+							case 3:
+								$result->$timestamp = $this->select('player.id, player.nickname, player.username')->player->get($result->$timestamp_by);
+								break;							
+							default:
+								break;
+						}
+					}
+				}
+			}
+		}
+
+		return $results;
+	}
+
+	/**
+	 * -------------------------
+	 * SELECT ROW - single
+	 * -------------------------
+	 */
+	public function get($id = null, $with_trashed = FALSE)
+	{
+		$this->db->from($this->table);
+		
+		if ($id !== null) {
+			$this->db->where([$this->table . '.id' => $id]);
+		}
+		if (! $with_trashed && $this->soft_delete) {
+			$this->db->where($this->table . '.deleted_at IS NULL');
+		}
+		$this->db->limit(1);
+		return $this->db->get()->row();
+	}
+
+	/**
+	 * -------------------------
+	 * CREATE METHOD
+	 * (modified to be bound on context)
+	 * -------------------------
+	 */
+	public function create($data)
+	{
+		return ($this->db->insert($this->table, $data)) ? $this->db->insert_id() : FALSE;
+	}
+
+	/**
+	 * -------------------------
+	 * UPDATE METHOD
+	 * (modified to be bound on context)
+	 * -------------------------
+	 */
+	public function update($data, $id = null)
+	{
+		if ($id) {
+			return $this->db->update($this->table, $data, [$this->table . '.id' => $id]);
+		} else {
+			return $this->db->update($this->table, $data);
+		}
+	}
+
+	/**
+	 * -------------------------
+	 * DELETE METHOD
+	 * -------------------------
+	 */
+	public function delete($id = null)
+	{
+		$data = $this->get($id);
+		if ($data) {
+			if ($this->soft_delete && $this->db->field_exists('deleted_at', $this->table)) {
+				$delete_data = [ $this->table . '.deleted_at' => date('Y-m-d H:i:s') ];
+				db_timestamp('deleted', $delete_data, $this->session_user->id, $this->session_user->type);
+				return $this->db->update($this->table, $delete_data, [$this->table . '.id' => $data->id]);
+			} else {
+				return $this->db->delete($this->table, [$this->table . '.id' => $data->id]);
+			}
+		} else {
+			return FALSE;
+		}
+	}
 }
